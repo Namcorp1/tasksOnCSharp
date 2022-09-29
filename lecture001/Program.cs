23f9b794500d606b97f8281c97b8cdd417a30355
namcorp1 <nikitamoskalev95@gmail.com>
2022-09-29 22:17:12 +0700

    Add gitignore

--- lecture001/Program.cs
+++ lecture001/Program.cs
@@ -1,40 +1,47 @@
-﻿Console.Clear();
-int ax = 40, ay = 1,
-    bx = 1, by = 40,
-    cx = 80, cy = 40;
-Console.SetCursorPosition(ax, ay);
-Console.WriteLine("*");
-
-Console.SetCursorPosition(bx, by);
-Console.WriteLine("*");
-
-Console.SetCursorPosition(cx, cy);
-Console.WriteLine("*");
-
-int x = ax, y = ay;
-
-int count = 0;
-
-while(count < 5)
+﻿internal class Program
 {
-    int point = new Random().Next(0,3);
-
-    if(point == 0)
+    private static void Main(string[] args)
     {
+        Console.Clear();
+        int ax = 1, ay = 1,
+            bx = 1, by = 40,
+            cx = 80, cy = 1;
+        Console.SetCursorPosition(ax, ay);
+        Console.WriteLine("+");
+
+        Console.SetCursorPosition(bx, by);
+        Console.WriteLine("+");
+
+        Console.SetCursorPosition(cx, cy);
+        Console.WriteLine("+");
+
+        int x = ax, y = ay;
+
+        int count = 0;
+
+        while(count < 5)
+        {
+        int point = new Random().Next(0,3);
+
+        if(point == 0)
+        {
         x = (x + ax) / 2;
         y = (y + ay) / 2;
-    }
-    if(point == 1)
-    {
+        }
+        if(point == 1)
+        {
         x = (x + bx) / 2;
         y = (y + by) / 2;
-    }
-    if(point == 2)
-    {
+        }
+        if(point == 2)
+        {
         x = (x + cx) / 2;
         y = (y + cy) / 2;
-    }
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("*");
-    count += 1;
+        }
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("*");
+        count += 1;
 }
+    }
+}
+
