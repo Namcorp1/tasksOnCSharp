6f876183ee19c28dea38a782b3c3942022e1036f
namcorp1 <nikitamoskalev95@gmail.com>
2022-09-30 22:27:57 +0700

    Add solution task 16

---
+++ tasksOnCSharp/task_16/Program.cs
@@ -0,0 +1,19 @@
+﻿// 16. Дано число. Проверить кратно ли оно 7 и 23
+Console.Write("Введите число, которое необходимо проверить на кратность 7 и 23: ");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+if (number % 7 == 0)
+{
+    if (number % 23 == 0)
+    {
+        Console.WriteLine($"Число {number} кратно 7 и 23!");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} некратно 7 и 23!");    
+    }
+}
+else
+{
+    Console.WriteLine($"Число {number} некратно 7 и 23!");
+}
