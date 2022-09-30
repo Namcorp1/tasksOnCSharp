e7df523f75cb10f71b48526043149ff95332b137
namcorp1 <nikitamoskalev95@gmail.com>
2022-09-30 22:21:09 +0700

    Add solution task 15

---
+++ tasksOnCSharp/task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15. Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число, третью цифру которого необходимо указать: ");
+string strNumber = Console.ReadLine();
+int countNumber = strNumber.Length;
+if(countNumber < 3)
+{
+    Console.WriteLine("Третьей цифры в числе нету!");
+}
+else
+{
+    Console.WriteLine($"{strNumber[2]} - третья цифра в числе {strNumber}");
+}
