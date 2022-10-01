8683d53383935afd2aeab9f04b34a2dbf203a382
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-01 12:04:47 +0700

    Add solution task 19

---
+++ tasksOnCSharp/task_19/Program.cs
@@ -0,0 +1,16 @@
+﻿// 19. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool x = true;
+bool y = true;
+
+Console.WriteLine($"x={x}, y={y}");
+Console.WriteLine(!( x || y));
+Console.WriteLine(!x && !y);
+
+if(!( x || y) == !x && !y)
+{
+    Console.WriteLine("Высказывание истинно!");
+}
+else
+{
+    Console.WriteLine("Высказывание ложно!");
+}
