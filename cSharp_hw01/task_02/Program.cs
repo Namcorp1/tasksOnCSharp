5a939cd91c0b156f4dfe7e5ca11695e982d7f7e4
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-02 10:52:27 +0700

    Add solution for task 02 on C# and correct diagram

--- cSharp_hw01/task_02/Program.cs
+++ cSharp_hw01/task_02/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, 
+которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.WriteLine("start");
+
+// блок с исходными данными
+int number1 = -9;
+int number2 = -3;
+int max = 0;
+int min = 0;
+
+//блок проверки равности чисел
+if (number1 == number2)
+{
+    Console.WriteLine("Числа одинаковы");
+}
+else
+{
+    if (number1 > number2)
+    {
+        max = number1;
+        min = number2;
+    }
+    else
+    {
+        max = number2;
+        min = number1;
+    }
+    Console.WriteLine($"Число {max} большее, число {min} меньшее.");
+}
+
+Console.WriteLine("end");
