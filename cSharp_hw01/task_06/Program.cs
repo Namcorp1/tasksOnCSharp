50ab57d9c015bd234b089fe85ed4b6917c9d9636
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-02 11:30:19 +0700

    Add solution for task 06 on C#

--- cSharp_hw01/task_06/Program.cs
+++ cSharp_hw01/task_06/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 6: Напишите программу, 
+которая на вход принимает число 
+и выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.WriteLine("start");
+
+// блок с исходными данными
+int number = 7;
+string result = "нет";
+
+// проверка числа на деление на 2 без остатка
+if (number % 2 == 0)
+{
+    result = "да";
+}
+Console.WriteLine(result);
+
+Console.WriteLine("end");
