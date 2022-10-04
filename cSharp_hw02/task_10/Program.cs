c16d5193850a3f9a9e2805e4bc50169d849f3add
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-04 20:59:07 +0700

    Add solution for task 10 on C# without construction IF

--- cSharp_hw02/task_10/Program.cs
+++ cSharp_hw02/task_10/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите метод, 
+который принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+// приглашение ко вводу
+int EnterCode(string input)
+{
+    Console.Write(input);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+// нахождение второй цифры в трёхзначном числе
+int Solution(int number)
+{
+    int secondNum = number / 10 % 10;
+    return secondNum;
+}
+// вывод результата
+void Print(int number, int secondNumber)
+{
+    string output = $"{secondNumber} - вторая цифра в числе {number}.";
+    Console.WriteLine(output);
+}
+
+// клиентский код. Start и end чисто для схожести с блок-схемой.
+Console.WriteLine("start");
+int number = EnterCode("Введите число: ");
+int secondNumber = Solution(number);
+Print(number, secondNumber);  
+Console.WriteLine("end");
