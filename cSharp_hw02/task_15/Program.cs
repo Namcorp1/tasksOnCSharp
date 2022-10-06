5393c3c7cf82c55a03047d687c6cfa1ec74099e0
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-06 21:47:04 +0700

    Add solution for task 15 on C# and correct diagram

--- cSharp_hw02/task_15/Program.cs
+++ cSharp_hw02/task_15/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿/*
+Задача 15: Напишите метод, который принимает на вход цифру,
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+// приглашение ко вводу
+int EnterCode(string helloText)
+{
+    Console.Write(helloText);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+string InitialResult(int numberDay)
+{
+    string result = "невыходной день.";
+    if (numberDay == 6 || numberDay == 7)
+    {
+        result = "выходной день!";
+    }
+    return result;
+}
+
+void PrintResult(string result, int numberDay)
+{
+    string output = $"{numberDay} - {result}";
+    Console.WriteLine(output);
+}
+
+Console.WriteLine("start");
+int numberDay = EnterCode("Введите номер недели: ");
+string result = InitialResult(numberDay);
+PrintResult(result, numberDay);
+Console.WriteLine("end");
