5b35a7b9b5a36425694cadef638d62683fbb500b
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-08 12:39:16 +0700

    Add diagram for task 22 and start coding solution on C#

---
+++ tasksOnCSharp/task_22/Program.cs
@@ -0,0 +1,16 @@
+﻿// 22. Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine("start");
+
+int number = 34543;
+int den1 = 1;
+int den2 = 10;
+int count = 1;
+int startNumeral = 0;
+int endNumeral = 0;
+
+while (number / den1 >= 10)
+{
+    den1 *= 10;
+    count += 1;
+}
