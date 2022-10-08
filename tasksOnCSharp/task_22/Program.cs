c72bb89b68f77660da5b1c3918a9d3c70e227e03
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-08 18:00:00 +0700

    Add code for task 22 on C# but not solution

--- tasksOnCSharp/task_22/Program.cs
+++ tasksOnCSharp/task_22/Program.cs
@@ -1,10 +1,11 @@
 ﻿// 22. Программа проверяет пятизначное число на палиндромом.
 
 Console.WriteLine("start");
+Console.Write("Введите число: ");
 
-int number = 34543;
+int number = Convert.ToInt32(Console.ReadLine());
 int den1 = 1;
-int den2 = 10;
+int den2 = 1;
 int count = 1;
 int startNumeral = 0;
 int endNumeral = 0;
@@ -14,3 +15,30 @@ while (number / den1 >= 10)
     den1 *= 10;
     count += 1;
 }
+Console.WriteLine(count);
+Console.WriteLine(den1);
+
+int n = count / 2;
+Console.WriteLine(n);
+while (count > n)
+{
+    startNumeral = number / (den1) - (number / den1) % (den1 / 10);
+    endNumeral = (number % den2 - number % (den2 * 10)) / (den2);
+    Console.WriteLine(startNumeral);
+    Console.WriteLine(endNumeral);
+
+    if (startNumeral == endNumeral)
+    {
+        count -= 1;
+        den1 /= 10;
+        den2 *= 10;
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не палиндром!");
+        break;
+    }
+
+}
+Console.WriteLine($"Число {number} палиндром!");
+Console.WriteLine("end");
