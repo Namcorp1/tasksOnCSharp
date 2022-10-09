3d7edd3803712507252551a48b65601411b4edda
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-09 18:40:01 +0700

    Add summ of array

---
+++ seminar003/array/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array1 = {1,2,3};
+int[] array2 = {3,2,1};
+int[] arrayResult = new int[3];
+
+int index = 0;
+int count = array1.Length;
+
+while(index < count)
+{
+    arrayResult[index] = array1[index] + array2[index];
+    index ++;
+}
+Console.WriteLine(string.Join(' ',arrayResult));
