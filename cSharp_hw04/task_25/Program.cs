f1d8a620363e44c7ca52311e6d1756cf3f0a08c9
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-12 21:18:10 +0700

    Add solution on C# for task 25

---
+++ cSharp_hw04/task_25/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 25: опишите метод, который принимает на вход 
+два числа (A и B) и возводит число A в целую степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+без math.pow
+*/
+
+// приглашение ко вводу
+int InputNumber (string text)
+{
+    Console.Write("Введите " + text + ": ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+// вычисление целой степени числа
+int ExpNumber (int a, int b)
+{
+    int count = 1;
+    int check = a;
+    while (count < b)
+    {
+        a *= check;
+        count += 1;
+    }
+    return a;
+}
+// вывод результата
+void PrintResult (int a, int b, int result)
+{
+    string output = $"{a} ^ {b} = {result}";
+    Console.WriteLine(output);
+}
+
+// клиентский код
+Console.WriteLine("start");
+int a = InputNumber ("число");
+int b = InputNumber ("степень");
+int result = ExpNumber (a, b);
+PrintResult (a, b, result);
+Console.WriteLine("end");
