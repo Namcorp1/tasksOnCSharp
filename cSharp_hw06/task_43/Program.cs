0bef234a54d7e6147a0751c41559043990c49e76
namcorp1 <nikitamoskalev95@gmail.com>
2022-10-19 22:46:01 +0700

    Add solution on C# for task 43

---
+++ cSharp_hw06/task_43/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+//приглашение ко вводу
+double InputData(string text)
+{
+    int data = 0;
+    bool check = false;
+    while (!check)
+    {
+        Console.Write($"Введите {text}: ");
+        string input = Console.ReadLine();
+        check = int.TryParse(input, out data);
+    }
+    return Convert.ToDouble(data);
+}
+
+//вычисление пересечения функций
+(double, double) Point(double k1, double b1, double k2, double b2)
+{
+    double x = 777;
+    double y = 666;
+
+    x = (b2 - b1) / (k1 - k2);
+    x = Math.Round(x, 2);
+    y = Math.Round(k1 * x + b1, 2);
+
+    return (x, y);
+}
+
+//метод проверки параллельности
+bool CheckParallel(double k1, double k2)
+{
+    return k1 == k2;
+}
+
+bool CheckOrdinate(double b1, double b2)
+{
+    return b1 == b2;
+}
+
+//вывод результата
+void PrintResult(double k1, double b1, double k2, double b2, double x, double y)
+{
+    string output = $"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})";
+    Console.WriteLine(output);
+}
+
+//клиентский код
+Console.WriteLine("start");
+double k1 = InputData("k1");
+double b1 = InputData("b1");
+double k2 = InputData("k2");
+double b2 = InputData("b2");
+if (CheckParallel(k1, k2) && CheckOrdinate(b1, b2)) Console.WriteLine("Линии совпадают!");
+else if (CheckParallel(k1, k2)) Console.WriteLine("Линии параллельны!");
+else
+{
+    (double x, double y) = Point(k1, b1, k2, b2);
+    PrintResult(k1, b1, k2, b2, x, y);
+}
+Console.WriteLine("end");
