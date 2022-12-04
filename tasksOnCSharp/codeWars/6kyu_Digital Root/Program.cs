ef58ac862a069fea201af569b21e2ff047f1de9b
namcorp1 <nikitamoskalev95@gmail.com>
2022-12-04 22:58:00 +0700

    Add solution for task CodeWars about Digital Root

---
+++ tasksOnCSharp/codeWars/6kyu_Digital Root/Program.cs
@@ -0,0 +1,42 @@
+﻿
+int Task(long n)
+{
+    long sum = 0;
+    while (n > 10)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    sum += n;
+    int res = Convert.ToInt32(sum);
+    if (res % 9 == 0)
+    {
+        int summ = 0;
+        while (res > 10)
+        {
+            summ += res % 10;
+            res /= 10;
+        }
+        summ += res;
+        res = summ;
+    }
+    if (res == 99 || res == 18) return 9;
+    else
+    {
+        res %= 9;
+    }
+    return res;
+    // long sum = 0;
+    // while (n > 10)
+    // {
+    //     sum += n % 10;
+    //     n /= 10;
+    // }
+    // sum += n;
+    // int res = Convert.ToInt32(sum);
+    // if (res>=10) res = Task(sum);
+    // return res;
+
+}
+int result = Task(99999999999);
+Console.WriteLine(result);
---
+++ tasksOnCSharp/codeWars/7kyu_NextPalindromicNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+There were and still are many problem in CW about palindrome numbers 
+and palindrome strings. We suposse that you know which kind of numbers they are. 
+If not, you may search about them using your favourite search engine.
+In this kata you will be given a positive integer, 
+val and you have to create the function next_pal()(nextPal Javascript) 
+that will output the smallest palindrome number higher than val.
+*/
+
+int NextPal(int val)
+{
+    int res = 0;
+    while (res != val)
+    {
+        res = 0;
+        val++;
+        int num = val;
+        int numeral = 0;
+        while (num >= 1)
+        {
+            numeral = num % 10;
+            res = res * 10 + numeral;
+            num /= 10;
+        }
+        if (res == val) return res;
+    }
+    return res;
+}
+
+int number = NextPal(191);
+Console.WriteLine(number);
